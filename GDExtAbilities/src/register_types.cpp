9c7540f56bc39ce98c6e782f16163901d2d23f9c
lSara-MM <sqmartinezm@gmail.com>
2025-09-05 20:05:02 +0200

    finish set up and first example plugin

---
+++ GDExtAbilities/src/register_types.cpp
@@ -0,0 +1,48 @@
+#include "register_types.hpp"
+
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+#include "gdExample.hpp"
+
+using namespace godot;
+
+void initialize_gdextabilities_plugin(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+
+    ClassDB::register_class<GDExample>();
+}
+
+void uninitialize_gdextabilities_plugin(ModuleInitializationLevel p_level)
+{
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+}
+
+extern "C"
+{
+    auto GDE_EXPORT gdextabilities_plugin_entry(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library,
+        GDExtensionInitialization* r_initialization) -> GDExtensionBool
+    {
+        godot::GDExtensionBinding::InitObject init_obj(
+            p_get_proc_address, p_library, r_initialization);
+
+        init_obj.register_initializer(initialize_gdextabilities_plugin);
+        init_obj.register_terminator(uninitialize_gdextabilities_plugin);
+
+        init_obj.set_minimum_library_initialization_level(
+            MODULE_INITIALIZATION_LEVEL_SCENE);
+
+        return init_obj.init();
+    }
+}
---
+++ GDExtAbilities/src/register_types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_gdextabilities_plugin(ModuleInitializationLevel p_level);
+void uninitialize_gdextabilities_plugin(ModuleInitializationLevel p_level);
