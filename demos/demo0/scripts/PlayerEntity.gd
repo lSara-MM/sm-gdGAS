f7242a91b29637086c8da10bfabbf4d73d31e1c9
lSara-MM <sqmartinezm@gmail.com>
2026-09-05 13:19:51 +0200

    nose

--- demos/demo0/scripts/PlayerEntity.gd
+++ demos/demo0/scripts/PlayerEntity.gd
@@ -6,7 +6,7 @@ class_name PlayerEntity
 @onready var ability_container = $AbilityContainer
 
 func _process(_delta: float) -> void:
-	if ability_container.iscooldown(Tags._Ability_Dash) && !ability_container.is_active(Tags._Ability_StaminaRegen) && attribute_container.get_attribute_current_value("CurrentStamina") < attribute_container.get_attribute_current_value("MaxStamina")  :
+	if ability_container.is_on_cooldown(Tags._Ability_Dash) && !ability_container.is_active(Tags._Ability_StaminaRegen) && attribute_container.get_attribute_current_value("CurrentStamina") < attribute_container.get_attribute_current_value("MaxStamina")  :
 		#if Input.is_action_just_pressed("lctrl"):
 			#ability_container.try_activate(Tags._Ability_StaminaRegen)
 		
